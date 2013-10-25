f55e17daf83283e4e0248607ac8c8cc2a309c49d
Mitesh Ashar <email@miteshashar.com>
2013-10-25 12:50:13 +0530

    Blanket commit. Apps get checked out in categorised folders. Further work on provisioning script and manage.py

--- hasdev/__init__.py
+++ hasdev/__init__.py
@@ -4,12 +4,21 @@ from yaml import load
 apps = load(file('instance/apps.yml', 'r'))
 settings = load(file('instance/settings.yml', 'r'))
 
+def dir(app):
+	return "%s/%s" % (apps[app]['type'], app)
+
+def make_parent(app):
+	if not os.path.exists(apps[app]['type']):
+		print "Directory %s doesn't exist. Creating %s..." % (apps[app]['type'], apps[app]['type'])
+		os.mkdir(apps[app]['type'])
+
 def clone(app):
 	try:
-		os.chdir(settings['options']['apps_path'])
+		make_parent(app)
+		os.chdir(apps[app]['type'])
 		if not os.path.exists("%s/.git" % app):
 			print "Cloning %s..." % app
-			os.system('date >> ../logs/git.log; git clone %s >> ../logs/git.log' % (settings['options']['repo_url'] % app))
+			os.system('git clone %s' % (settings['options']['repo_url'] % app))
 		os.chdir("..")
 	except KeyboardInterrupt:
 		print "Cloning interrupted. Removing %s..." % app
@@ -19,41 +28,41 @@ def clone(app):
 		exit()
 
 def setup(app):
-	app_path = "%s/%s" % (settings['options']['apps_path'], app)
-	remote_app_path = "/vagrant/%s" % app_path
+	remote_app_path = "/vagrant/%s" % dir(app)
 	try:
 		print "Setting up %s for development..." % app
 		run(app, "sudo python setup.py develop")
 	except KeyboardInterrupt:
 		print "Setup interrupted. Removing %s..." % app
-		os.system('rm -rf %s' % (app_path))
+		os.system('rm -rf %s' % dir(app))
 		exit()
 
 def install(app):
 	if app == "all":
 		for app in apps:
-			if 'dev_setup' not in apps[app] or apps[app]['dev_setup'] is False:
+			if apps[app]['type'] is not "dependencies":
 				install(app)
-	elif app in apps and 'dev_setup' in apps[app] and apps[app]['dev_setup']:
+	elif app in apps:
 		print "Installing %s..." % app
 		clone(app)
-		setup(app)
-	elif app in apps and ('dev_setup' not in apps[app] or apps[app]['dev_setup'] is False):
-		print "Installing %s..." % app
-		clone(app)
-		update_requirements(app)
+		if apps[app]['type'] == "dependencies":
+			setup(app)
+		elif apps[app]['type'] != "boilerplates":
+			update_requirements(app)
 	else:
 		print "%s not configured"
 
 def update(app):
 	pull(app)
-	update_requirements(app)
+	if apps[app]['type'] == "dependencies":
+		setup(app)
+	elif apps[app]['type'] != "boilerplates":
+		update_requirements(app)
 
 def pull(app):
-	path = "%s/%s" % (settings['options']['apps_path'], app)
-	if os.path.exists(path):
-		os.chdir(path)
-		os.system("date >> ../logs/git.log; git pull >> ../logs/git.log;")
+	if os.path.exists(dir(app)):
+		os.chdir(dir(app))
+		os.system("git pull")
 		update_requirements(app)
 		os.chdir("../..")
 	else:
@@ -61,17 +70,16 @@ def pull(app):
 
 def update_requirements(app):
 	print "Setting up requirements..."
-	if os.path.exists("%s/%s/requirements.txt" % (settings['options']['apps_path'], app)):
+	if os.path.exists("%s/requirements.txt" % dir(app)):
 		run(app, "sudo pip install -r requirements.txt --upgrade")
 	else:
 		print "requirements.txt not found for %s" % app
 
 def run(app, command):
-	path = "%s/%s" % (settings['options']['apps_path'], app)
-	if os.path.exists(path):
-		remote_app_path = "/vagrant/%s" % path
+	if os.path.exists(dir(app)):
+		remote_app_path = "/vagrant/%s" % dir(app)
 		command = "cd %s; %s" % (remote_app_path, command)
-		os.system("vagrant ssh -c 'date >> /vagrant/logs/remote-commands.log; echo \"%s\" >> /vagrant/logs/remote-commands; %s >> /vagrant/logs/remote-commands'" % (command, command))
+		os.system("vagrant ssh -c '%s'" % command)
 	else:
 		print "App does not exist..."
 
@@ -80,16 +88,13 @@ def init():
 	YN = {"yes":True, "y":True, "ye":True, "no":False, "n":False}
 	while choice not in YN:
 		choice = raw_input("Do you want to setup all apps after the VM is created and setup[y/n]? ").lower()
-	if not os.path.exists(settings['options']['apps_path']):
-		print "Directory %s doesn't exist. Creating %s..." % settings['options']['apps_path']
-		os.mkdir(settings['options']['apps_path'])
 	if not os.path.exists(".vagrant/machines/default/virtualbox/id"):
 		print "Setting up virtual machine..."
 		os.system("vagrant up --provision")
 	else:
 		print "Virtual machine already exists..."
 	for app in apps:
-		if 'dev_setup' in apps[app] and apps[app]['dev_setup'] and not os.path.isdir("%s/.git" % app):
+		if apps[app]['type'] == "dependencies":
 			install(app)
 	if YN[choice]:
 		install("all")
--- hasdev/init.sh
+++ hasdev/init.sh
@@ -1,41 +1,78 @@
 #! /usr/bin/env bash
 
-mkdir logs
+function hr(){
+	echo ""
+	echo "====================================="
+	echo ""
+}
 
+hr
 echo "Running Ubuntu update..."
-apt-get update > logs/apt-get-update.log
-
+hr
+apt-get update
+hr
+echo "Installing make..."
+hr
+apt-get install -y make -qq
+hr
 echo "Installing curl..."
-apt-get install -y curl > /vagrant/logs/apt-get-install.log
+hr
+apt-get install -y curl -qq
+hr
+echo "Installing unzip..."
+hr
+apt-get install -y unzip -qq
+hr
 echo "Installing python-dev..."
-apt-get install -y python-dev >> /vagrant/logs/apt-get-install.log
+hr
+apt-get install -y python-dev -qq
+hr
+echo "Installing python setuptools ..."
+hr
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+python ez_setup.py
+rm ez_setup.py
+rm setuptools-*
+hr
+echo "Installing python pip ..."
+hr
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+rm get-pip.py
+hr
+echo "Installing postgress..."
+hr
+apt-get install -y postgresql postgresql-contrib python-psycopg2 libpq-dev build-essential
+hr
+echo "Setting up postgres..."
+hr
+sudo -u postgres createuser -SDRl hasgeek
+sudo -u postgres psql -c "ALTER USER hasgeek WITH PASSWORD 'hasgeek'"
+hr
 echo "Installing redis-server..."
-apt-get install -y redis-server >> /vagrant/logs/apt-get-install.log
+hr
+apt-get install -y redis-server -qq
+hr
+echo "Installing openjdk-7-jre-headless..."
+hr
+apt-get install -y openjdk-7-jre-headless -qq
+hr
 echo "Installing nodejs..."
-apt-get install -y nodejs >> /vagrant/logs/apt-get-install.log
+hr
+apt-get install -y nodejs -qq
+hr
 # https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager#ubuntu-mint-elementary-os
 # Please report any errors you may face with nodejs on the Issue Tracker for hasweb
-echo "Installing openjdk-7-jre..."
-apt-get install -y openjdk-7-jre >> /vagrant/logs/apt-get-install.log
-echo "Installing make..."
-apt-get install -y make >> /vagrant/logs/apt-get-install.log
-echo "Installing unzip..."
-apt-get install -y unzip >> /vagrant/logs/apt-get-install.log
 echo "Installing npm..."
-apt-get install -y npm >> /vagrant/logs/apt-get-install.log
-
-echo "Installing python distribute ..."
-curl -O http://python-distribute.org/distribute_setup.py
-python distribute_setup.py > /vagrant/logs/python-setups.log
-rm distribute_setup.py
-
-echo "Installing python pip ..."
-curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-python get-pip.py >> /vagrant/logs/python-setups.log
-rm get-pip.py
-
+hr
+apt-get install -y npm -qq
+hr
 echo "Installing compass..."
-gem install compass > /vagrant/logs/compass-install.log
+hr
+gem install compass
+hr
 
-echo "Installing uglifyjs..."
-npm install uglifyjs > /vagrant/logs/uglifyjs-install.log
+# echo "Installing uglifyjs..."
+# npm install uglify-js
+# uglifyjs installs are just failing because of some dependency on nodejs app source-map.
+# hr
