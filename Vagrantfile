7caf0b452a094548739d681575313f24685660df
Mitesh Ashar <email@miteshashar.com>
2013-10-25 14:26:03 +0530

    Converted files to spaces.

--- Vagrantfile
+++ Vagrantfile
@@ -10,29 +10,29 @@ VAGRANTFILE_API_VERSION = "2"
 app_hosts = []
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
-  config.vm.network :private_network, ip: settings['vm']['host_ip']
-  config.vm.hostname = settings['vm']['hostname']
+    config.vm.box = "precise64"
+    config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+    config.vm.network :private_network, ip: settings['vm']['host_ip']
+    config.vm.hostname = settings['vm']['hostname']
 
-  apps.each do |app, opts|
-    if opts['port']
-      config.vm.network :forwarded_port, guest: opts['port'], host: opts['port']
-      if opts['host']
-        app_hosts.push(opts['host'])
-      else
-        app_hosts.push(settings['options']['base_host'] % app)
-      end
+    apps.each do |app, opts|
+        if opts['port']
+            config.vm.network :forwarded_port, guest: opts['port'], host: opts['port']
+            if opts['host']
+                app_hosts.push(opts['host'])
+            else
+                app_hosts.push(settings['options']['base_host'] % app)
+            end
+        end
     end
-  end
 
-  config.vm.provision "shell", path: "hasdev/init.sh"
+    config.vm.provision "shell", path: "hasdev/init.sh"
 
-  # Adding /etc/hosts entries, `vagrant plugin install vagrant-hostsupdater`
-  if defined? VagrantPlugins::HostsUpdater
-    config.hostsupdater.aliases = app_hosts
-  else
-    puts "vagrant-hostsupdater not installed, so not updating /etc/hosts"
-  end
+    # Adding /etc/hosts entries, `vagrant plugin install vagrant-hostsupdater`
+    if defined? VagrantPlugins::HostsUpdater
+        config.hostsupdater.aliases = app_hosts
+    else
+        puts "vagrant-hostsupdater not installed, so not updating /etc/hosts"
+    end
 
 end
--- hasdev/__init__.py
+++ hasdev/__init__.py
@@ -5,96 +5,96 @@ apps = load(file('instance/apps.yml', 'r'))
 settings = load(file('instance/settings.yml', 'r'))
 
 def dir(app):
-	return "%s/%s" % (apps[app]['type'], app)
+    return "%s/%s" % (apps[app]['type'], app)
 
 def make_parent(app):
-	if not os.path.exists(apps[app]['type']):
-		print "Directory %s doesn't exist. Creating %s..." % (apps[app]['type'], apps[app]['type'])
-		os.mkdir(apps[app]['type'])
+    if not os.path.exists(apps[app]['type']):
+        print "Directory %s doesn't exist. Creating %s..." % (apps[app]['type'], apps[app]['type'])
+        os.mkdir(apps[app]['type'])
 
 def clone(app):
-	try:
-		make_parent(app)
-		os.chdir(apps[app]['type'])
-		if not os.path.exists("%s/.git" % app):
-			print "Cloning %s..." % app
-			os.system('git clone %s' % (settings['options']['repo_url'] % app))
-		os.chdir("..")
-	except KeyboardInterrupt:
-		print "Cloning interrupted. Removing %s..." % app
-		os.system('rm -rf %s' % app)
-		os.chdir("..")
-		print "Removed %s..." % app
-		exit()
+    try:
+        make_parent(app)
+        os.chdir(apps[app]['type'])
+        if not os.path.exists("%s/.git" % app):
+            print "Cloning %s..." % app
+            os.system('git clone %s' % (settings['options']['repo_url'] % app))
+        os.chdir("..")
+    except KeyboardInterrupt:
+        print "Cloning interrupted. Removing %s..." % app
+        os.system('rm -rf %s' % app)
+        os.chdir("..")
+        print "Removed %s..." % app
+        exit()
 
 def setup(app):
-	remote_app_path = "/vagrant/%s" % dir(app)
-	try:
-		print "Setting up %s for development..." % app
-		run(app, "sudo python setup.py develop")
-	except KeyboardInterrupt:
-		print "Setup interrupted. Removing %s..." % app
-		os.system('rm -rf %s' % dir(app))
-		exit()
+    remote_app_path = "/vagrant/%s" % dir(app)
+    try:
+        print "Setting up %s for development..." % app
+        run(app, "sudo python setup.py develop")
+    except KeyboardInterrupt:
+        print "Setup interrupted. Removing %s..." % app
+        os.system('rm -rf %s' % dir(app))
+        exit()
 
 def install(app):
-	if app == "all":
-		for app in apps:
-			if apps[app]['type'] is not "dependencies":
-				install(app)
-	elif app in apps:
-		print "Installing %s..." % app
-		clone(app)
-		if apps[app]['type'] == "dependencies":
-			setup(app)
-		elif apps[app]['type'] != "boilerplates":
-			update_requirements(app)
-	else:
-		print "%s not configured"
+    if app == "all":
+        for app in apps:
+            if apps[app]['type'] is not "dependencies":
+                install(app)
+    elif app in apps:
+        print "Installing %s..." % app
+        clone(app)
+        if apps[app]['type'] == "dependencies":
+            setup(app)
+        elif apps[app]['type'] != "boilerplates":
+            update_requirements(app)
+    else:
+        print "%s not configured"
 
 def update(app):
-	pull(app)
-	if apps[app]['type'] == "dependencies":
-		setup(app)
-	elif apps[app]['type'] != "boilerplates":
-		update_requirements(app)
+    pull(app)
+    if apps[app]['type'] == "dependencies":
+        setup(app)
+    elif apps[app]['type'] != "boilerplates":
+        update_requirements(app)
 
 def pull(app):
-	if os.path.exists(dir(app)):
-		os.chdir(dir(app))
-		os.system("git pull")
-		update_requirements(app)
-		os.chdir("../..")
-	else:
-		print "App does not exist..."
+    if os.path.exists(dir(app)):
+        os.chdir(dir(app))
+        os.system("git pull")
+        update_requirements(app)
+        os.chdir("../..")
+    else:
+        print "App does not exist..."
 
 def update_requirements(app):
-	print "Setting up requirements..."
-	if os.path.exists("%s/requirements.txt" % dir(app)):
-		run(app, "sudo pip install -r requirements.txt --upgrade")
-	else:
-		print "requirements.txt not found for %s" % app
+    print "Setting up requirements..."
+    if os.path.exists("%s/requirements.txt" % dir(app)):
+        run(app, "sudo pip install -r requirements.txt --upgrade")
+    else:
+        print "requirements.txt not found for %s" % app
 
 def run(app, command):
-	if os.path.exists(dir(app)):
-		remote_app_path = "/vagrant/%s" % dir(app)
-		command = "cd %s; %s" % (remote_app_path, command)
-		os.system("vagrant ssh -c '%s'" % command)
-	else:
-		print "App does not exist..."
+    if os.path.exists(dir(app)):
+        remote_app_path = "/vagrant/%s" % dir(app)
+        command = "cd %s; %s" % (remote_app_path, command)
+        os.system("vagrant ssh -c '%s'" % command)
+    else:
+        print "App does not exist..."
 
 def init():
-	choice = ""
-	YN = {"yes":True, "y":True, "ye":True, "no":False, "n":False}
-	while choice not in YN:
-		choice = raw_input("Do you want to setup all apps after the VM is created and setup[y/n]? ").lower()
-	if not os.path.exists(".vagrant/machines/default/virtualbox/id"):
-		print "Setting up virtual machine..."
-		os.system("vagrant up --provision")
-	else:
-		print "Virtual machine already exists..."
-	for app in apps:
-		if apps[app]['type'] == "dependencies":
-			install(app)
-	if YN[choice]:
-		install("all")
+    choice = ""
+    YN = {"yes":True, "y":True, "ye":True, "no":False, "n":False}
+    while choice not in YN:
+        choice = raw_input("Do you want to setup all apps after the VM is created and setup[y/n]? ").lower()
+    if not os.path.exists(".vagrant/machines/default/virtualbox/id"):
+        print "Setting up virtual machine..."
+        os.system("vagrant up --provision")
+    else:
+        print "Virtual machine already exists..."
+    for app in apps:
+        if apps[app]['type'] == "dependencies":
+            install(app)
+    if YN[choice]:
+        install("all")
