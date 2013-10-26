87c72eb5782689c4f5cea51942fe134107426cbd
Mitesh Ashar <email@miteshashar.com>
2013-10-26 21:16:41 +0530

    Made commands easier. No more usage of argparse. Eliminated unnecessary run & provision commands.

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import argparse
 from yaml import load
 import hasdev
 
@@ -9,21 +8,15 @@ apps = load(file('instance/apps.yml', 'r'))
 settings = load(file('instance/settings.yml', 'r'))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Manage your development setup')
-    parser.add_argument('command', help='Command to be run. Supported commands: init, install, update, run, provision')
-    parser.add_argument('--app', help='The app to take a particular action on. Works with run, install & update.')
-    parser.add_argument('--execute', help='The command to be executed remotely. Works with run.')
-    args = parser.parse_args()
+    args = sys.argv
+    while len(args) < 3:
+        args.append(None)
     
-    if args.command == 'init':
+    if args[1] == 'init':
         hasdev.init()
-    elif args.command == 'install':
-        hasdev.install(args.app)
-    elif args.command == 'update':
-        hasdev.update(args.app)
-    elif args.command == 'run':
-        hasdev.run(args.app, args.execute)
-    elif args.command == 'provision':
-        os.system("vagrant provision")
+    elif args[1] == 'install':
+        hasdev.install(args[2])
+    elif args[1] == 'update':
+        hasdev.update(args[2])
     else:
         print "Invalid command"
